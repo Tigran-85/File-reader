f95a6bd3c5da232206e0dc1dddfb9fab2d58afdb
Tigran Poghosyan <tigranpoghosyan94@gmail.com>
2022-11-30 23:17:57 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,154 @@
+const fs = require('fs/promises');
+
+(async () => {
+
+     // commands
+     const CREATE_FILE = "create a file";
+     const DELETE_FILE = "delete the file";
+     const RENAME_FILE = "rename the file";
+     const ADD_TO_FILE = "add to the file";
+
+    const createFile = async (path) => {
+        
+        try {
+            // we want to check whether or not we already have that file
+            const existingFileHandler = await fs.open(path, "r");
+            existingFileHandler.close();
+
+            return console.log(`The file ${path} already exists`);
+        } catch (e) {
+            // we don't have the file, now we should create it
+            const newFileHandle = await fs.open(path, "w");
+            console.log("A new file was successfully created.");
+            newFileHandle.close();
+        }
+        
+    }
+
+    const deleteFile = async (path) => {
+        
+        try {
+            // we want to delete file
+            await fs.unlink(path);
+
+            console.log(`File ${path} was deleted succesfully`);
+        } catch (e) {
+            if (e.code === "ENOENT") {
+                console.log(`file ${path} does not exists`)
+            } else {
+                console.log("An error occured while removing the file", e);
+            }
+            
+        }
+        
+    }
+
+    const renameFile = async (oldPath, newPath) => {
+
+        try {
+            await fs.rename(oldPath, newPath);
+            console.log(`The file ${oldPath} was successfully renamed`);
+
+        } catch (e) {
+            if (e.code === "ENOENT") {
+                console.log(`file ${oldPath} or destination doesn't exists`)
+            } else {
+                console.log("An error occured while renaming the file", e);
+            }
+        }
+    }
+
+    let addedContent;
+
+    const addToFile = async (path, content) => {
+        if (addedContent === content) {
+            return;
+        }
+        try {
+            const fileHandler = await fs.open(path, "a");
+
+            fileHandler.write(content);
+            addedContent = content;
+            console.log(`Adding to ${path}`);
+            console.log(`Content ${content} was added successfully`);
+            fileHandler.close();
+        } catch (e) {
+            console.log("An error occured while adding the file", e);
+        }
+    }
+
+    const commandFileHandler = await fs.open("./command.txt", "r");
+
+    commandFileHandler.on("change", async () => {
+         // get the size of our file
+
+         const size = (await commandFileHandler.stat()).size;
+
+         // allocate our buffer with the size of the file
+
+         const buff = Buffer.alloc(size);
+
+         // the location at which we want to start filling our buffer
+         const offset = 0;
+
+         // how many bytes we want to read
+         const length = buff.byteLength;
+
+         // the position that we want to start reading the file from
+         const position = 0;
+
+         // we always want to read the whole content (from the begining all the way to the end)
+         await commandFileHandler.read(buff, offset, length, position);
+
+         const command = buff.toString("utf-8");
+
+         // create a file
+         // create a file <path>
+        if (command.includes(CREATE_FILE)) {
+            const filePath = command.substring(CREATE_FILE.length + 1);
+            createFile(filePath);
+        }
+
+        // delete a file
+        // delete the file <path>
+
+        if (command.includes(DELETE_FILE)) {
+            const filePath = command.substring(DELETE_FILE.length + 1);
+            deleteFile(filePath);
+        }
+
+        // rename the file
+        // rename the file <path> to <new-path>
+
+        if (command.includes(RENAME_FILE)) {
+            const _indx = command.indexOf(" to ");
+            const oldFilePath = command.substring(RENAME_FILE.length + 1, _indx);
+            const newFilePath = command.substring(_indx + 4);
+            renameFile(oldFilePath, newFilePath);
+        }
+
+        // add to file
+        // add to the file <path> this content: <content>
+
+        if (command.includes(ADD_TO_FILE)) {
+            const _indx = command.indexOf(" this content: ")
+            const filePath = command.substring(ADD_TO_FILE.length + 1, _indx);
+            const content = command.substring(_indx + 15);
+
+            addToFile(filePath, content);
+        }
+    })
+
+    // watcher...
+   const watcher = fs.watch("./command.txt");
+
+    for await (const event of watcher) {
+        if (event.eventType === "change") {
+            commandFileHandler.emit("change");
+        
+        }
+
+       
+    }
+ 
+})();
